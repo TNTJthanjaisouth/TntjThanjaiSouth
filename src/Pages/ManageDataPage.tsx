c6cd812b75a71f21b00815f894dbf6174ff9906a
TNTJthanjaisouth <filestntj@gmail.com>
2025-11-12 17:42:41 +0530

    Update ManageDataPage.tsx

--- src/Pages/ManageDataPage.tsx
+++ src/Pages/ManageDataPage.tsx
@@ -6,8 +6,8 @@ const ManageDataPage = () => {
   const navigate = useNavigate();
   return (
     <div className="space-y-8">
-      <header className="bg-green-600 text-white py-4 px-4 sm:px-8  shadow-md flex justify-between items-center">
-        <h1 className="text-xl sm:text-2xl font-bold">Dhayi List</h1>
+      <header className="bg-green-600 text-white py-4 px-4 sm:px-8  shadow-md flex justify-between items-center fixed top-0 w-full z-[100]">
+        <h1 className="text-xl sm:text-2xl font-bold">Dhayikal List</h1>
 
         <button
           className="bg-white text-green-600 px-3 py-2 rounded-md text-md font-bold hover:bg-green-100 inline-flex"
@@ -16,9 +16,10 @@ const ManageDataPage = () => {
           <ArrowLeft className="mr-3" /> back
         </button>
       </header>
-
-      <ImageManager />
-      <DhayiNamesManager />
+      <div className="lg:mx-24 ">
+        <ImageManager />
+        <DhayiNamesManager />
+      </div>
     </div>
   );
 };
